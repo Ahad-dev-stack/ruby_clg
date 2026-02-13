ac47477452abc26655844d7cab3d615173d6fd9c
Ahad-dev-stack <abdul1204ahad@gmail.com>
2026-02-13 11:27:08 +0530

    Update nStr.rb

--- nStr.rb
+++ nStr.rb
@@ -1,9 +1,9 @@
 print "Enter a string: "
-str = gets
+str = gets.chomp
 
 print "Enter a non-negative integer: "
 n = gets.to_i
 
-new_str = str * n
+new_str = ([str] * n).join(" ")
 
 puts "Result: #{new_str}"
