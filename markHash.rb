ba3333af34a1363c5090c4cf3bfb414824e96374
Ahad-dev-stack <abdul1204ahad@gmail.com>
2026-02-13 11:29:00 +0530

    Update markHash.rb

--- markHash.rb
+++ markHash.rb
@@ -1,52 +1,25 @@
 marks = {}
 
-puts "Enter number of subjects:"
+print "Enter number of subjects: "
 n = gets.to_i
 
 n.times do
-  puts "Enter subject name:"
+  print "Subject name: "
   subject = gets.chomp
 
-  puts "Enter marks for #{subject}:"
-  score = gets.to_i
-
-  marks[subject] = score
+  print "Marks for #{subject}: "
+  marks[subject] = gets.to_i
 end
 
-puts "\nSubjects and Marks:"
-marks.each do |subject, score|
-  puts "#{subject} => #{score}"
-end
+puts "\nAll Subjects and Marks:"
+puts marks
 
-total = 0
-marks.each_value do |m|
-  total += m
-end
-
+total = marks.values.sum
 puts "\nTotal Marks = #{total}"
-
-puts "\nSubjects:"
-puts marks.keys.inspect
-
-puts "\nMarks:"
-puts marks.values.inspect
 
 passed = marks.select { |_, score| score >= 20 }
 puts "\nPassed Subjects:"
-puts passed.inspect
+puts passed
 
-puts "\nEnter a subject to delete:"
-del_subject = gets.chomp
-marks.delete(del_subject)
-
-puts "After deletion:"
-puts marks.inspect
-
-extra = { "Sports" => 20 }
-marks.merge!(extra)
-
-puts "\nAfter merging extra marks:"
-puts marks.inspect
-
-puts "\nInverted Hash (marks => subject):"
-puts marks.invert.inspect
+print "\nEnter subject to delete: "
+del = gets.chomp
