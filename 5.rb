0abd44ea91cc80c405d186d84cfa4e61f603f9ee
Ahad-dev-stack <abdul1204ahad@gmail.com>
2026-03-28 19:55:59 +0530

    Update 5.rb

--- 5.rb
+++ 5.rb
@@ -1,11 +1,16 @@
-a = gets.chomp.to_i
-b = gets.chomp.to_i
-c = gets.chomp.to_i
+print "Enter a value"
+a = gets.to_i
+
+print "Enter b value"
+b = gets.to_i
+
+print "Enter c value"
+c = gets.to_i
 
 if a>b && a>c
-	puts "a is greater"
+    puts "#{a} is greater"
 elsif b>a && b>c
-	puts "b is greater"
+    puts "#{b} is greater"
 else
-	puts "c is greater"
+    puts "#{c} is greater"
 end
